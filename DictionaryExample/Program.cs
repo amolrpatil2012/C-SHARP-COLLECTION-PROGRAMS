7016bca5c6636c56a4aff40e17cc5e61eecf40a3
amol <amolrpatil2012@gmail.com>
2025-01-17 09:59:53 +0530

    Dictionary Programs

---
+++ DictionaryExample/Program.cs
@@ -0,0 +1,55 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+       Dictionary<int,string> map = new Dictionary<int,string>();
+
+        // Adding elements to Dictionary
+        map.Add(1, "Sachin");
+        map.Add(2, "Rahul");
+        map.Add(3, "Prakash");
+        
+        // Traversing Dictionary
+        foreach( var entry in map )         // entry is combination of key and value
+        {
+            Console.WriteLine( "Key : " + entry.Key  + "Value : " + entry.Value);
+        }
+
+        foreach (KeyValuePair<int,string> entry in map)         
+        {
+            Console.WriteLine("Key : " + entry.Key + "Value : " + entry.Value);
+        }
+
+        // Search Operation
+
+        Console.WriteLine( " If Key 1 Exists : " + map.ContainsKey(1));
+        Console.WriteLine( " If Value Rahul Exists : " + map.ContainsValue("Rahul"));
+
+        // Remove Operation
+        map.Remove(1);          // remove entry where key is 1
+        map.Clear();            // remove all elements of map
+
+
+        // Get All Keys
+
+        ICollection<int> keys = map.Keys;
+        Console.WriteLine("All Keys");
+        foreach (int key in keys ) 
+            Console.WriteLine(key);
+
+        // Get All Values
+        ICollection<string> values = map.Values;
+        foreach (var entry in values)
+            Console.WriteLine(entry);
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ MapProblem/Program.cs
@@ -0,0 +1,48 @@
+﻿
+class Emp
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
+class Dept
+{
+    public string Id { get; set; }
+    public string Name { get; set; }
+   
+}
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Emp emp1 = new Emp() { Id = 1, Name = "Suresh" };
+        Emp emp2 = new Emp() { Id = 2, Name = "Sachin" };
+        Emp emp3 = new Emp() { Id = 3, Name = "Rahul" };
+        Emp emp4 = new Emp() { Id = 4, Name = "Rohit" };
+
+        Dept dept1 = new Dept() { Id = "IT", Name = "IT Dept" };
+        Dept dept2 = new Dept() { Id = "ACC", Name = "ACCOUNTS Dept" };
+
+        Dictionary<Emp,Dept> d = new Dictionary<Emp,Dept>();    
+        d.Add(emp1, dept1);         // emp1 [ 1, Suresh ] --- dept1 [ IT ]
+        d.Add(emp2 , dept2);        // emp2 [ 2 , Sachin ] --- dept2 [ ACC ]
+        d.Add(emp3, dept1);         // emp3 [ 3 , Rahul ] --- dept1 [ IT ]
+        d.Add(emp4 , dept2);        // emp4 [ 4 , Rohit ] --- dept2 [ ACC ]
+       
+        Console.WriteLine("Enter Id of Emp");
+        int Eid = int.Parse(Console.ReadLine());
+
+        foreach (var entry in d) 
+        {
+            Emp emp = entry.Key;
+            Dept dept = entry.Value;
+
+            if ( emp.Id == Eid)
+            {
+                Console.WriteLine( dept.Name );
+            }
+        }
+
+
+    }
+}
